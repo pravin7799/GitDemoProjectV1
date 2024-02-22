abf629cbea0f78663bc10abc16ab5174589427f8
Pravin.Gardade <Pravin.Gardade@ENCDAPPUNLT0067>
2024-02-22 12:29:44 +0530

    first commit

---
+++ src/demo/demo.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          String name="Pravin";
+          
+          if(name == "Pravin") {
+        	  System.out.println("He is talented guy");
+          }
+	}
+
+}
